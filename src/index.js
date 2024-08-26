8e1858bfeaf2e2c5a100bd6ce6a2f8894b9a76ff
Alec Lang <lostlang@icloud.com>
2024-08-26 21:40:03 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+	let count = bracketsConfig.map((el) => {
+		if (el[0] == el[1]) {
+			return true;
+		}
+		return false;
+	});
+	let newStr = "";
+
+	let r = str.split("").reduce((acc, el) => {
+		bracketsConfig.forEach((brasket, i) => {
+			if (brasket[0] == el) {
+				if (count[i]) {
+					if (newStr[newStr.length - 1] == el) {
+						newStr = newStr.slice(0, -1);
+					} else {
+						newStr += el;
+					}
+				} else {
+					newStr += el;
+				}
+			} else if (brasket[1] == el) {
+				if (newStr[newStr.length - 1] == brasket[0]) {
+					newStr = newStr.slice(0, -1);
+				} else {
+					acc = false;
+				}
+			}
+		});
+
+		return acc;
+	}, true);
+
+	if (newStr.length > 0) {
+		return false;
+	}
+
+	return r;
+};
